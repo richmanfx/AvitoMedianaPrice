7aedc7b7761b469580bbddd52eedcce4c6162e48
Aleksandr Jashhuk <richmanfx@yandex.ru>
2019-08-21 19:31:59 +0300

    Логирование в консоль через логгер.

--- src/main/java/ru/r5am/SelenideSetUp.java
+++ src/main/java/ru/r5am/SelenideSetUp.java
@@ -1,14 +1,20 @@
 package ru.r5am;
 
 import com.codeborne.selenide.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SelenideSetUp {
 
+    private static final Logger log = LogManager.getLogger();
+
     public void selenideStart() {
+        log.debug("===> Start method: " + Thread.currentThread().getStackTrace()[1].getMethodName());
         Configuration.pageLoadStrategy = "normal";
         Configuration.timeout = 1000 * 7;           // из секунд в мс
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+        Configuration.screenshots = false;      // Делать ли скриншот средсвами Selenide
     }
 
 }
