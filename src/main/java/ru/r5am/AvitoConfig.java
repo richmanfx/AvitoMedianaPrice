422d018eb9aa3ff6aa2f586f05301537041c3113
Aleksandr Jashhuk <richmanfx@yandex.ru>
2019-08-21 20:08:21 +0300

    Работа через удалённый WebDriver. Конфигурационные параметры вынесены в файл "app.config".

---
+++ src/main/java/ru/r5am/AvitoConfig.java
@@ -0,0 +1,20 @@
+package ru.r5am;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({ "file:app.config" })
+public interface AvitoConfig extends Config {
+
+    // Использование удалённых браузеров, таких как "Selenium Grid" или "Solenoid"
+    @DefaultValue("true")
+    Boolean remoteBrowserFlag();
+    @DefaultValue("127.0.0.1")
+    String remoteSeleniumHub();
+    @DefaultValue("4444")
+    String remoteSeleniumHubPort();
+
+    // Размер окна браузера
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+}
--- src/main/java/ru/r5am/SelenideSetUp.java
+++ src/main/java/ru/r5am/SelenideSetUp.java
@@ -1,20 +1,38 @@
 package ru.r5am;
 
 import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SelenideSetUp {
 
     private static final Logger log = LogManager.getLogger();
+    private static AvitoConfig avitoConfig = ConfigFactory.create(AvitoConfig.class);
 
     public void selenideStart() {
         log.debug("===> Start method: " + Thread.currentThread().getStackTrace()[1].getMethodName());
         Configuration.pageLoadStrategy = "normal";
         Configuration.timeout = 1000 * 7;           // из секунд в мс
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = avitoConfig.browserSize();
         Configuration.screenshots = false;      // Делать ли скриншот средсвами Selenide
+
+        log.info("===> remoteBrowserFlag: " + avitoConfig.remoteBrowserFlag());
+
+        // Для работы через Selenium Grid
+        if (avitoConfig.remoteBrowserFlag()) {
+
+            String remoteSeleniumHub = avitoConfig.remoteSeleniumHub();
+            log.info("Remote Selenium Hub: " + avitoConfig.remoteSeleniumHub());
+
+            String remoteSeleniumHubPort = avitoConfig.remoteSeleniumHubPort();
+            log.info("Remote port of Selenium Hub: " + remoteSeleniumHubPort);
+
+            Configuration.remote = String.format("http://%s:%s/wd/hub", remoteSeleniumHub, remoteSeleniumHubPort);
+            Configuration.browserCapabilities.setCapability("enableVNC", true);
+        }
+
     }
 
 }
